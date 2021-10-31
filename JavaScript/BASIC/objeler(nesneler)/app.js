94ed4b740f8e21e1bf6965bbfaf729a74eae8425
Mehmet <mkaymaz93@gmail.com>
2021-10-31 12:11:18 +0300

    dom

---
+++ JavaScript/BASIC/objeler(nesneler)/app.js
@@ -0,0 +1,29 @@
+/* var isim = "Mehmet";
+var yas = 28 ;
+var gozRengi = "Ela";
+
+document.write(isim+" "+yas+" "+gozRengi); */
+
+var insan1 = {
+    name : "Mehmet",
+    age : 28 ,
+    eyes : "Ela"
+};
+console.log(insan1.name+" "+insan1.age+" "+insan1.eyes);
+console.log(insan1.name, insan1.age, insan1.eyes);
+
+console.log(insan1.name.length);
+
+// function insan(isim, yasi, gozRengi){
+//     this.isim = isim;
+//     this.yasi = yasi;
+//     this.gozRengi = gozRengi;
+//     this.isimDegistir = function(yeniIsim){ 
+//         this.isim = yeniIsim;
+//     }
+// }
+
+// var i1 = new insan("Ahmet", 20, "Kahverengi");
+// console.log(i1.isim+" "+i1.yasi+" "+i1.gozRengi);
+// i1.isimDegistir("Kamil");
+// console.log(i1.isim+" "+i1.yasi+" "+i1.gozRengi);
